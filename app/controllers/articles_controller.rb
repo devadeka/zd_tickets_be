39ac5d4492fc6630120825909d770948b574eb4e
Devatanu Deka <deva.deka@gmail.com>
2019-03-12 18:34:16 +1100

    request stats added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
+  PER_PAGE = 10
+
   def index
-    @articles = Article.all.paginate(page: params[:page], per_page: 10)
-    render json: @articles
+    page = (params[:page] || 1).to_i
+    page_count = (Article.count.to_f/PER_PAGE).round
+    articles = Article.all.paginate(page: page, per_page: PER_PAGE)
+    response = {page: page, page_count: page_count, articles: articles}
+    render json: response
   end
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,19 +1,96 @@
 require 'rails_helper'
 
-RSpec.describe 'Articles API', type: :request do 
-  let!(:articles) { create_list(:article, 20) }
-  let(:page_params) { {page: 1} }
-  describe 'GET /articles' do
-    before { get '/articles', params: page_params }
+RSpec.describe 'Articles API', type: :request do
+  let!(:articles) { create_list(:article, 27) }
+  let(:first_article_id) { articles.first['external_id'] }
+  let(:last_article_id) { articles.last['external_id'] }
 
-    it 'returns articles' do
-      @json = JSON.parse(response.body)
-      expect(@json).not_to be_empty
-      expect(@json.size).to eq(10)
+  describe 'GET /articles' do
+    context 'when no page specified' do
+      before { get '/articles' }
+
+      it 'returns articles' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles).not_to be_empty
+        expect(@articles.size).to eq(10)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the expected articles in page' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles.first['external_id']).to eq(first_article_id)
+      end
+
+      it 'returns the correct page' do
+        @json = JSON.parse(response.body)
+        expect(@json['page']).to eq(1)
+      end
+
+      it 'returns the correct page_count' do
+        @json = JSON.parse(response.body)
+        expect(@json['page_count']).to eq(3)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
+    context 'when first page is specified' do
+      let(:page_params) { {page: 1} }
+
+      before { get '/articles', params: page_params }
+
+      it 'returns articles' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles).not_to be_empty
+        expect(@articles.size).to eq(10)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the expected articles in page' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles.first['external_id']).to eq(first_article_id)
+      end
+
+      it 'returns the correct page' do
+        @json = JSON.parse(response.body)
+        expect(@json['page']).to eq(1)
+      end
+    end
+
+    context 'when last page is specified' do
+      let(:page_params) { {page: 3} }
+
+      before { get '/articles', params: page_params }
+
+      it 'returns articles' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles).not_to be_empty
+        expect(@articles.size).to eq(7)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the expected articles in page' do
+        @json = JSON.parse(response.body)
+        @articles = @json['articles']
+        expect(@articles.last['external_id']).to eq(last_article_id)
+      end
+
+      it 'returns the correct page' do
+        @json = JSON.parse(response.body)
+        expect(@json['page']).to eq(3)
+      end
     end
   end
 end
