55f7501603eb67e263f8f16217e7f7386331c313
Devatanu Deka <deva.deka@gmail.com>
2019-03-13 17:03:08 +1100

    caching with redis works - can see resposnes for page request stored in redis

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,11 @@ class ArticlesController < ApplicationController
 
   def index
     page = (params[:page] || 1).to_i
-    page_count = (Article.count.to_f/PER_PAGE).round
-    articles = Article.all.paginate(page: page, per_page: PER_PAGE)
-    response = {page: page, page_count: page_count, articles: articles}
-    render json: response
+    Rails.cache.fetch(['zd_faqs', page], expires_in: 3.minutes) do
+      page_count = (Article.count.to_f/PER_PAGE).round
+      articles = Article.all.paginate(page: page, per_page: PER_PAGE)
+      response = {page: page, page_count: page_count, articles: articles}
+      render json: response
+    end
   end
 end
